73a9d09129d99a68794a259afe7a4e942f8f8f5c
Carmine Scarpitta <cscarpit@cisco.com>
2024-11-01 22:24:47 -0400

    Simplify host addressing
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- config/h0/config.sh
+++ config/h0/config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-ip addr add 2001:db8:0::1/64 dev lo
-ip addr add 2001:db8:10:a0::2/64 dev eth1
+ip addr add 2001:db8:0::2/64 dev eth1
 
-ip -6 route add default via 2001:db8:10:a0::1
+ip -6 route add default via 2001:db8:0::1
