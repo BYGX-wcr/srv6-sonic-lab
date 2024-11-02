d52bd23df1f4f0a7a36bb2ade74147e006a0cdf1
Carmine Scarpitta <cscarpit@cisco.com>
2024-11-02 03:14:02 -0400

    Change host addressing
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- config/h1/config.sh
+++ config/h1/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-ip addr add 2001:db8:1::2/64 dev eth1
+ip addr add 2001:db8:1::1/64 dev eth1
 
-ip -6 route add default via 2001:db8:1::1
+ip -6 route add default via 2001:db8:1::2
