7b097691790d426d52688b3852a8343840871824
Carmine Scarpitta <cscarpit@cisco.com>
2024-11-04 08:05:19 -0600

    Fix fib_multipath_hash_fields
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- config/sonic20/config.sh
+++ config/sonic20/config.sh
@@ -5,4 +5,4 @@ ip link set sr0 up
 sysctl -w net.vrf.strict_mode=1
 sysctl -w net.ipv6.seg6_flowlabel=1
 sysctl -w net.ipv6.fib_multipath_hash_policy=3
-sysctl -w net.ipv6.fib_multipath_hash_fields=11
+sysctl -w net.ipv6.fib_multipath_hash_fields=0x0080
