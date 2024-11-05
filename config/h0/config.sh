ea2c7e0ba32770871d2deb7120b675d7e10444c6
ahabdels <ahabdels@cisco.com>
2024-11-05 14:27:52 +0100

    fix hash fields and install tcpdump and iperf on hosts
    Signed-off-by: ahabdels <ahabdels@cisco.com>

--- config/h0/config.sh
+++ config/h0/config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+sudo apt-get install -y tcpdump
+sudo apt-get install -y iperf
+
 ip addr add 2001:db8:0::1/64 dev eth1
 
 ip -6 route add default via 2001:db8:0::2
--- config/h1/config.sh
+++ config/h1/config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
-ip addr add 2001:db8:1::1/64 dev eth1
+sudo apt-get install -y tcpdump
+sudo apt-get install -y iperf
 
+ip addr add 2001:db8:1::1/64 dev eth1
 ip -6 route add default via 2001:db8:1::2
