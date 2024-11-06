e88f5f7e3712bbf14ad5592a54f6a2d2b8b1adbe
BYGX-wcr <wcr@live.cn>
2024-11-06 23:41:29 +0000

    add extra steps to make sure ipv6 working on the hosts

--- config/h0/config.sh
+++ config/h0/config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
 apt-get install -y tcpdump
-apt-get install -y iperf
+apt-get install -y iperf3
+
+echo 0 > /proc/sys/net/ipv6/conf/eth1/disable_ipv6
+ip link set dev eth1 down
+ip link set dev eth1 up
 
 ip addr add 2001:db8:0::1/64 dev eth1
 
+ip -6 route del default
 ip -6 route add default via 2001:db8:0::2
