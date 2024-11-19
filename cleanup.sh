c60548fa91fb4cbd5fb7a9e05a4de85b88b230b4
BYGX-wcr <wcr@live.cn>
2024-11-19 23:41:44 +0000

    reformat cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,30 +1,30 @@
 function remove_sonic_switch {
-	    SWNAME=$1
+	SWNAME=$1
 
-	        docker stop sw-$SWNAME && docker rm sw-$SWNAME
-		    docker stop $SWNAME && docker rm $SWNAME
+	docker stop sw-$SWNAME && docker rm sw-$SWNAME
+	docker stop $SWNAME && docker rm $SWNAME
 
-		        for srv in `seq 0 31`; do
+	for srv in `seq 0 31`; do
 
-				        SRV="$SWNAME-srv$srv"
+	SRV="$SWNAME-srv$srv"
 
-					        ip netns del $SRV
+	ip netns del $SRV
 
-						    done
-					   }
+	done
+}
 
 
-					   function remove_host {
-						       SWNAME=$1
+function remove_host {
+	SWNAME=$1
 
-						           docker stop $SWNAME && docker rm $SWNAME
-						   }
+	docker stop $SWNAME && docker rm $SWNAME
+}
 
-						   remove_sonic_switch sonic10
-						   remove_sonic_switch sonic20
-						   remove_sonic_switch sonic21
-						   remove_sonic_switch sonic11
-						   remove_sonic_switch a0
-						   remove_sonic_switch a1
-						   remove_host h0
-						   remove_host h1
+remove_sonic_switch sonic10
+remove_sonic_switch sonic20
+remove_sonic_switch sonic21
+remove_sonic_switch sonic11
+remove_sonic_switch a0
+remove_sonic_switch a1
+remove_host h0
+remove_host h1
