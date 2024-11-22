e9586fe2554633267ba3913d57b752a1f85f06cc
BYGX-wcr <wcr@live.cn>
2024-11-22 19:00:17 +0000

    use ethtool to disable checksum calculation offloading on hosts

--- config/h1/config.sh
+++ config/h1/config.sh
@@ -3,10 +3,12 @@
 apt-get update
 apt-get install -y tcpdump
 apt-get install -y iperf
+apt-get install -y ethtool
 
 echo 0 > /proc/sys/net/ipv6/conf/eth1/disable_ipv6
 ip link set dev eth1 down
 ip link set dev eth1 up
+ethtool -K eth1 tx off
 
 ip addr add 2001:db8:1::1/64 dev eth1
 ip -6 route del default
